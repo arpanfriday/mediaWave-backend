99745c539b2fbefb8656ea55a9841d0cd15a50df
Arpan Maity <arpanriju2000@gmail.com>
2025-02-15 17:31:07 +0530

    refactor: rename subscription controller methods for clarity

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,4 +1,4 @@
-import mongoose, { isValidObjectId } from "mongoose";
+import mongoose from "mongoose";
 import { User } from "../models/user.model.js";
 import { Subscription } from "../models/subscription.model.js";
 import { ApiError } from "../utils/ApiError.js";
@@ -11,13 +11,13 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 });
 
 // controller to return subscriber list of a channel
-const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+const getSubscribersOfChannel = asyncHandler(async (req, res) => {
     const { channelId } = req.params;
 });
 
 // controller to return channel list to which user has subscribed
-const getSubscribedChannels = asyncHandler(async (req, res) => {
+const getChannelsSubscribedTo = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params;
 });
 
-export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
+export { toggleSubscription, getSubscribersOfChannel, getChannelsSubscribedTo };
