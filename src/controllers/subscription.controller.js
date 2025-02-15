7168ba0e9534aeb1994deedf64584879da9dce3b
Arpan Maity <arpanriju2000@gmail.com>
2025-02-15 17:33:01 +0530

    feat: implement toggle subscription functionality in subscription controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -7,7 +7,58 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
     const { channelId } = req.params;
-    // TODO: toggle subscription
+
+    const result = await Subscription.aggregate([
+        {
+            $match: {
+                $and: [
+                    { channel: new mongoose.Types.ObjectId(channelId) },
+                    { subscriber: new mongoose.Types.ObjectId(req.user?._id) },
+                ],
+            },
+        },
+    ]);
+
+    if (result?.length === 0) {
+        Subscription.create([
+            {
+                channel: new mongoose.Types.ObjectId(channelId),
+                subscriber: new mongoose.Types.ObjectId(req.user?._id),
+            },
+        ])
+            .then((result) => {
+                return res
+                    .status(200)
+                    .json(
+                        new ApiResponse(
+                            200,
+                            { subscribed: true, result: result },
+                            "You Subscribed to the channel"
+                        )
+                    );
+            })
+            .catch((error) => {
+                return new ApiError(400, error);
+            });
+    } else {
+        Subscription.deleteOne({
+            _id: new mongoose.Types.ObjectId(result?.[0]?._id),
+        })
+            .then((result) => {
+                return res
+                    .status(200)
+                    .json(
+                        new ApiResponse(
+                            200,
+                            { subscribed: false, result: [result] },
+                            "You Unsubscribed to the channel"
+                        )
+                    );
+            })
+            .catch((error) => {
+                return new ApiError(400, error);
+            });
+    }
 });
 
 // controller to return subscriber list of a channel
