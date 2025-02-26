1207c54ae13130ca6f0c7aa945ab49c0e7d634aa
Arpan Maity <arpanriju2000@gmail.com>
2025-02-26 18:26:32 +0530

    feat: enhance video retrieval with query, sorting, and pagination support

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -9,8 +9,71 @@ import {
 } from "../utils/cloudinary.js";
 
 const getAllVideos = asyncHandler(async (req, res) => {
-    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
-    //TODO: get all videos based on query, sort, pagination
+    const {
+        page = 1,
+        limit = 10,
+        query = "",
+        sortBy = "title",
+        sortType = "desc",
+        userId,
+    } = req.query;
+
+    if (!req.user) {
+        throw new ApiError(401, "User needs to be logged in");
+    }
+
+    const match = {
+        ...(query ? { title: { $regex: query, $options: "i" } } : {}),
+        ...(userId ? { owner: mongoose.Types.ObjectId(userId) } : {}),
+    };
+
+    const videosAggregation = Video.aggregate([
+        {
+            $match: match,
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "videosByOwner",
+            },
+        },
+        {
+            $project: {
+                videoFile: 1,
+                thumbnail: 1,
+                title: 1,
+                description: 1,
+                duration: 1,
+                views: 1,
+                isPublished: 1,
+                owner: {
+                    $arrayElemAt: ["$videosByOwner", 0],
+                },
+            },
+        },
+        {
+            $sort: {
+                [sortBy]: sortType === "desc" ? -1 : 1,
+            },
+        },
+    ]);
+
+    const options = {
+        page: parseInt(page, 10),
+        limit: parseInt(limit, 10),
+    };
+
+    const videos = await Video.aggregatePaginate(videosAggregation, options);
+
+    if (!videos?.docs?.length) {
+        throw new ApiError(404, "Videos are not found");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, videos, "Videos fetched successfully"));
 });
 
 const publishAVideo = asyncHandler(async (req, res) => {
