d30e983c07889b2ad737d57144a0150dc6f6801d
Arpan Maity <arpanriju2000@gmail.com>
2025-03-01 01:43:13 +0530

    feat: enhance content-length logging format in morgan middleware

--- src/middlewares/logger.middleware.js
+++ src/middlewares/logger.middleware.js
@@ -4,13 +4,20 @@ import morgan from "morgan";
 const stream = {
     write: (message) => logger.info(message.trim()),
 };
-morgan.token("content-length-bytes", (req, res) => {
-    const contentLength = res.get("content-length");
-    return contentLength ? `${contentLength} bytes` : "0 bytes";
+morgan.token("content-length", (req, res) => {
+    const contentLength = parseInt(res.get("content-length")) || 0;
+
+    if (contentLength < 1024) {
+        return `${contentLength} B`;
+    } else if (contentLength < 1024 * 1024) {
+        return `${(contentLength / 1024).toFixed(2)} KB`;
+    } else {
+        return `${(contentLength / (1024 * 1024)).toFixed(2)} MB`;
+    }
 });
 
 const morganConfig = morgan(
-    ":method :url :status :content-length-bytes - :response-time ms",
+    ":method :url :status :content-length - :response-time ms",
     {
         stream,
     }
