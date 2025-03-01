d0c611f71ef7682797bd06dcb0b319072fb43ee1
Arpan Maity <arpanriju2000@gmail.com>
2025-03-01 18:32:34 +0530

    feat: add logging functionality to ApiResponse class for improved message tracking

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,9 +1,17 @@
+import logger from "./logger.js";
+
 class ApiResponse {
     constructor(statusCode, data, message = "Success") {
         this.statusCode = statusCode;
         this.data = data;
         this.message = message;
         this.success = statusCode < 400;
+        this.logMessage();
+    }
+
+    logMessage() {
+        // Log the error message along with the stack trace
+        logger.info(this.message);
     }
 }
 
