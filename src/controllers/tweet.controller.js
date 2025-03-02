510974e13e815776a76c68ce4ac6831359d85a0b
Arpan Maity <arpanriju2000@gmail.com>
2025-03-02 17:38:57 +0530

    refactor: streamline tweet controller methods with async/await and improved error handling

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -5,94 +5,101 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const createTweet = asyncHandler(async (req, res, next) => {
+const createTweet = asyncHandler(async (req, res) => {
     const { content } = req.body;
-    Tweet.create({
-        content: content,
-        owner: new mongoose.Types.ObjectId(req.user?._id),
-    })
-        .then((result) => {
-            return res
-                .status(201)
-                .json(
-                    new ApiResponse(
-                        200,
-                        { result },
-                        `tweet ${result._id} created by ${req.user?._id}`
-                    )
-                );
-        })
-        .catch((error) => {
-            next(new ApiError(500, "Cannot create the record: " + error));
-        });
-});
-
-const getUserTweets = asyncHandler(async (req, res, next) => {
-    const userId = req.params;
-
-    Tweet.find({ owner: new mongoose.Types.ObjectId(userId?.userId) })
-        .then((result) => {
-            return res
-                .status(200)
-                .json(
-                    new ApiResponse(
-                        200,
-                        result,
-                        `fetched all the tweets of user ${userId?.userId}`
-                    )
-                );
-        })
-        .catch((error) => {
-            next(new ApiError(500, "error fetching data: ", error));
-        });
-});
-
-const updateTweet = asyncHandler(async (req, res, next) => {
-    const { content } = req.body;
-    const { tweetId } = req.params;
-
-    if (!content) throw new ApiError(400, `content is missing`);
-
-    Tweet.updateOne(
-        { _id: tweetId },
-        {
+    try {
+        const result = await Tweet.create({
             content: content,
             owner: new mongoose.Types.ObjectId(req.user?._id),
-        }
-    )
-        .then((result) => {
-            return res
-                .status(202)
-                .json(
-                    new ApiResponse(
-                        200,
-                        { result },
-                        `tweet ${tweetId} updated successfully`
-                    )
-                );
-        })
-        .catch((error) => {
-            next(new ApiError(500, "cannot update the record: " + error));
         });
+        return res
+            .status(201)
+            .json(
+                new ApiResponse(
+                    200,
+                    { result },
+                    `tweet ${result._id} created by ${req.user?._id}`
+                )
+            );
+    } catch (error) {
+        throw new ApiError(500, error);
+    }
 });
 
-const deleteTweet = asyncHandler(async (req, res, next) => {
-    const { tweetId } = req.params;
-    Tweet.deleteOne({ _id: tweetId })
-        .then((result) => {
-            return res
-                .status(204)
-                .json(
-                    new ApiResponse(
-                        200,
-                        { result },
-                        `tweet ${tweetId} deleted successfully`
-                    )
-                );
-        })
-        .catch((error) => {
-            next(new ApiError(500, "cannot delete the record: " + error));
+const getUserTweets = asyncHandler(async (req, res) => {
+    const userId = req.params;
+    if (!mongoose.Types.ObjectId.isValid(userId?.userId))
+        throw new ApiError(400, "user ID invalid");
+
+    try {
+        const result = await Tweet.find({
+            owner: new mongoose.Types.ObjectId(userId?.userId),
         });
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    result,
+                    `fetched all the tweets of user ${userId?.userId}`
+                )
+            );
+    } catch (error) {
+        throw new ApiError(500, "error fetching data: ", error);
+    }
+});
+
+const updateTweet = asyncHandler(async (req, res) => {
+    const { content } = req.body;
+    const { tweetId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(tweetId))
+        throw new ApiError(400, "invalid tweet ID");
+    if (!content) throw new ApiError(400, `content is missing`);
+
+    try {
+        const result = await Tweet.updateOne(
+            { _id: tweetId },
+            {
+                content: content,
+                owner: new mongoose.Types.ObjectId(req.user?._id),
+            }
+        );
+        return res
+            .status(202)
+            .json(
+                new ApiResponse(
+                    200,
+                    { result },
+                    `tweet ${tweetId} updated successfully`
+                )
+            );
+    } catch (error) {
+        throw new ApiError(500, "cannot update the record: " + error);
+    }
+});
+
+const deleteTweet = asyncHandler(async (req, res) => {
+    const { tweetId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(tweetId))
+        throw new ApiError(400, "invalid tweet id");
+
+    try {
+        const result = await Tweet.deleteOne({ _id: tweetId });
+
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    { result },
+                    `tweet ${tweetId} deleted successfully`
+                )
+            );
+    } catch (error) {
+        throw new ApiError(500, "cannot delete the record: " + error);
+    }
 });
 
 export { createTweet, getUserTweets, updateTweet, deleteTweet };
